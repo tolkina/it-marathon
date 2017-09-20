46a0c4f4adc30f5cf67c6e1610e8ed77104621a3
aleksandra_tolkina@mail.ru <tolkina_aa_14@mf.grsu.by>
2017-09-20 12:58:44 +0300

    Add logic in method getTopOfMostCommonWords

--- src/main/java/instinctools/marathon/it/java/TextServiceImpl.java
+++ src/main/java/instinctools/marathon/it/java/TextServiceImpl.java
@@ -4,17 +4,50 @@ import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 @Service("textService")
 public class TextServiceImpl implements TextService {
+
+    private String excludeConjPronPrep(String text) throws FileNotFoundException {
+        File prepositionsFile = new File("D:\\Studing\\java\\src\\main\\resources\\prepositions");
+        List<String> prepositions = new ArrayList<>();
+        Scanner scannerPrepositions = new Scanner(prepositionsFile);
+        while (scannerPrepositions.hasNextLine())
+            prepositions.add(scannerPrepositions.nextLine());
+        for (String preposition : prepositions) {
+            String regex = String.format("(?i)[^А-я]%s[^А-я]", preposition);
+            text = text.replaceAll(regex, " ");
+        }
+
+        File conjunctionsFile = new File("D:\\Studing\\java\\src\\main\\resources\\conjunctions");
+        List<String> conjunctions = new ArrayList<>();
+        Scanner scannerConjuctions = new Scanner(conjunctionsFile);
+        while (scannerConjuctions.hasNextLine())
+            conjunctions.add(scannerConjuctions.nextLine());
+        for (String conjunction : conjunctions) {
+            String regex = String.format("(?i)[^А-я]%s[^А-я]", conjunction);
+            text = text.replaceAll(regex, " ");
+        }
+
+        File pronounsFile = new File("D:\\Studing\\java\\src\\main\\resources\\pronouns");
+        List<String> pronouns = new ArrayList<>();
+        Scanner scannerPronouns = new Scanner(pronounsFile);
+        while (scannerPronouns.hasNextLine())
+            pronouns.add(scannerPronouns.nextLine());
+        for (String pronoun : pronouns) {
+            String regex = String.format("(?i)[^А-я]%s[^А-я]", pronoun);
+            text = text.replaceAll(regex, " ");
+        }
+        return text;
+    }
 
     private void verifyFileExtension(MultipartFile file) throws UnsupportedFileTypeException {
         String filename = file.getOriginalFilename();
@@ -27,7 +60,25 @@ public class TextServiceImpl implements TextService {
     public List<String> getTopOfMostCommonWords(MultipartFile file) throws UnsupportedFileTypeException, IOException {
         verifyFileExtension(file);
         String text = new String(file.getBytes());
-        return null;
+        text = excludeConjPronPrep(text.toLowerCase());
+
+        Pattern wordPattern = Pattern.compile("[А-я]+-?[А-я]+");
+        List<String> tokens = new ArrayList<>();
+        Matcher matcher = wordPattern.matcher(text);
+        while (matcher.find()) {
+            String word = matcher.group();
+            tokens.add(word);
+        }
+        Map<String, Long> map = tokens.stream()
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        tokens = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        return tokens;
     }
 
     private boolean isOpenBracket(Character character) {
